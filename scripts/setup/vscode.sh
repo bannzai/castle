170da690ae8373090f0a9d17b84405182f31ab4e
bannzai <kingkong999yhirose@gmail.com>
2019-07-07 16:05:00 +0900

    :sparkles: Add vscode script

---
+++ scripts/setup/vscode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eu
+
+# For VSCode vim plugin
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+
