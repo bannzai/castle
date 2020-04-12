b77a167f296e06455c2be540a23e9b3e7ee09640
bannzai <kingkong999yhirose@gmail.com>
2020-04-12 13:50:12 +0900

    :recycle: fix home dir

--- scripts/setup/prezto.sh
+++ scripts/setup/prezto.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 set -eu
 
-PREZTO_DIR="/.homesick/repos/castle/home/.zprezto"
+PREZTO_DIR="~/.homesick/repos/castle/home/.zprezto"
 for rcfile in $PREZTO_DIR/runcoms/*; do
   ln -s "$rcfile" "$PREZTO_DIR/.${rcfile:t}"
 done
