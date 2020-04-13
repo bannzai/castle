db2f0199dff514b5a7b9ad21d4c440a7c5824273
hirose yudai <kingkong999yhirose@gmail.com>
2020-04-13 13:19:27 +0900

    Update go_library.sh

--- scripts/setup/go_library.sh
+++ scripts/setup/go_library.sh
@@ -2,12 +2,10 @@
 set -eu
 
 COMMANDS=(
-  "go get -u github.com/derekparker/delve/cmd/dlv"
   "go get github.com/golang/mock/gomock"
   "go get -u github.com/golang/dep/cmd/dep"
   "go get -u github.com/motemen/gore"
   "go get github.com/cweill/gotests/..."
-  "go get -u github.com/sourcegraph/go-langserver"
 )
 for ((i = 0; i < ${#COMMANDS[@]}; i++))
 do
