06ec5c06622d76ec1cb3fae847f4f8ebc1cc06bc
“bannzai” <yudai-hirose@cookpad.com>
2020-04-13 19:30:01 +0900

    :memo: fix prezto scripts

--- scripts/setup/prezto.sh
+++ scripts/setup/prezto.sh
@@ -6,8 +6,12 @@ setopt EXTENDED_GLOB
 PREZTO_DIR="$HOME/.homesick/repos/castle/home/.zprezto"
 ls $PREZTO_DIR
 for rcfile in $PREZTO_DIR/runcoms/^README.md(.N); do
-  echo "symlink $rcfile for $PREZTO_DIR/.${rcfile:t}"
-  # ln -s "$rcfile" "$PREZTO_DIR/.${rcfile:t}"
+  echo "mv $HOME/${rcfile:t} $HOME/${rcfile:t}.bk"
+  set +e
+  mv "$HOME/${rcfile:t}" "$HOME/${rcfile:t}.bk"
+  set -e
+  echo "ln -s $rcfile for $HOME/.${rcfile:t}"
+  ln -s "$PREZTO_DIR/.${rcfile:t}" 
 done
 echo "end prezto setup"
 
