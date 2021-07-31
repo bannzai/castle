2f0bc9f0203b5f9775b14d772d3ffaaa331bf4d7
bannzai <kingkong999yhirose@gmail.com>
2021-08-01 00:14:10 +0900

    Revert ":memo: add xctemplates"
    This reverts commit 973f197a730034bad1db0069772293c6bf79d6f1.

--- home/xctemplates/copy_templates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cwd=$(cd $(dirname $0); pwd -P)
-
-mkdir -p ~/Library/Developer/Xcode/Templates
-for file in ${cwd}/*.xctemplate; do
-    ln -fs $file ~/Library/Developer/Xcode/Templates/
-done
